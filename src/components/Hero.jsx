f95737748119bbc2558b055aca6165af8d5bc320
Sahil <leviack629@gmail.com>
2023-03-18 13:15:13 +0530

    modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,22 +6,22 @@ const Hero = () => {
 
 
     <>
-      <section className='bg-gradient-to-r from-cyan-300  to-cyan-100 relative min-h-[80vh] overflow-hidden' >
+      <section className='bg-gradient-to-r from-cyan-300  to-cyan-100 relative min-h-[80vh] overflow-hidden font-Roboto' >
         <div className=' max-w-[1400px] py-8 bg-transparent m-auto sm:grid grid-cols-2  float  md:py-3 px-4 items-end relative' >
           <div className='md:w-[130%] text-center sm:text-left sm:pb-60' >
             <span className='font-semibold text-[#FC5E2C]' >Enjoy worry-free pet parenthood with our <br /> comprehensive care service.</span>
             <h1 className='font-black text-black text-4xl md:text-7xl py-8' >Your lovely pet <br /> needs a lovely haircut.</h1>
-            <p className='py-6 text-gray-700 max-w-md md:mb-10' >We care for your pet as much as you do. Book an appointment today to visit us.</p>
+            <p className='py-6 text-gray-700 max-w-md md:mb-10 font-bold' >We care for your pet as much as you do. Book an appointment today to visit us.</p>
             <a href="/" className='py-3 px-6 rounded-full text-[14px] font-semibold bg-teal-700 text-white items-center' >
               Book Appointment
             </a>
           </div>
           <div className='' >
-            <img src={heroImg} alt="" className='max-h-md mb-0 mx-auto md:mb-20 lg:mb-28 xl:mb-40' />
+            <img src={heroImg} alt="" className='max-h-md mb-0 mx-auto md:mb-10 lg:mb-28 xl:mb-40' />
           </div>
         </div>
         <div className='absolute block w-full bottom-0 ' >
-          <svg xmlns="http://www.w3.org/svg" viewBox="0 0 1340 220" className=""><path fill="#fff" fillOpacity="1" d="M0,128L80,144C160,160,320,192,480,181.3C640,171,800,117,960,106.7C1120,96,1280,128,1360,144L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+          <svg xmlns="http://www.w3.org/svg" viewBox="0 0 1440 270" className=""><path fill="#fff" fillOpacity="1" d="M0,128L80,144C160,160,320,192,480,181.3C640,171,800,117,960,106.7C1120,96,1280,128,1360,144L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         </div>
       </section>
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import one from '../assets/1.png'
 import two from '../assets/2.png'
-import three from '../assets/3.png'
+import three from '../assets/ff.png'
 import four from '../assets/fours.png'
 import five from '../assets/5.png'
 import six from '../assets/6.png'
@@ -18,8 +18,8 @@ const Services = () => {
 {/* 1st */}
         <div className='grid grid-cols-2 bg-gradient-to-r overflow-hidden max-h-96  from-teal-400 to-teal-300 rounded-md py-3   items-center relative' >
           <div className='w-[130%] px-4' >
-            <h2 className='text-4xl font-bold text-white ' >Vet Clinic</h2>
-            <p className='py-6 text-gray-700' >A range of services to keep your furry friends healthy and happy.</p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins' >Vet Clinic</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl' >A range of services to keep your furry friends healthy and happy.</p>
             <button className='py-2 px-4 rounded-full text-[14px] font-semibold bg-teal-700 text-white flex max-w-[200px] items-center' >
               Book Appointment
               <span className=' bg-white text-black rounded-full ml-2' > <GoChevronRight className='text-2xl' /></span>
@@ -36,8 +36,8 @@ const Services = () => {
             <img src={two} alt="" className='relative -left-3' />
           </div>
           <div className='w-[130%]  relative left-[-30%] px-4 ' >
-            <h2 className='text-4xl font-black text-white' >Grooming</h2>
-            <p className='py-6 text-gray-700' >We maintain your pet’s appearance and hygiene </p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins'  >Grooming</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl' >We maintain your pet’s appearance and hygiene </p>
             <button className='py-2 px-4 rounded-full text-[14px] font-semibold bg-teal-700 text-white flex max-w-[200px] items-center' >
               Book Appointment
               <span className=' bg-white text-black rounded-full ml-2' > <GoChevronRight className='text-2xl' /></span>
@@ -49,11 +49,11 @@ const Services = () => {
 {/* 3rd */}
         <div className='grid grid-cols-2 bg-gradient-to-r float max-h-96 from-cyan-300 to-cyan-400 rounded-md py-3 px-4 items-center relative' >
           <div className='md:mt-16'>
-            <img src={three} alt="/" className='relative -left-3 object-cover' />
+            <img src={three} alt="/" className='relative -left-5 -bottom-5 w-[55%] object-cover md:w-[60%]' />
           </div>
           <div className='w-[130%]  relative left-[-30%]' >
-            <h2 className='text-4xl font-black text-white' >Creche</h2>
-            <p className='py-6 text-gray-700' >Leave your pet with us for the day and let them enjoy a fun and comfortable stay while you're away</p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins'  >Creche</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl' >Leave your pet with us for the day and let them enjoy a fun and comfortable stay while you're away</p>
           </div>
         </div>
 
@@ -61,22 +61,23 @@ const Services = () => {
         {/* 4th */}
         <div className='grid grid-cols-2 bg-gradient-to-r float max-h-96 from-green-300 to-green-400 rounded-md py-3  items-center relative' >
           <div className='w-[130%] px-4' >
-            <h2 className='text-4xl font-black text-white' >Pet Store</h2>
-            <p className='py-6 text-gray-700' >Your one-stop shop for all pet’s needs, from toys to treats, beds to bowls, and everything in between</p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins'  >Pet Store</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl'  >Your one-stop shop for all pet’s needs, from toys to treats, beds to bowls, and everything in between</p>
           </div>
-          <div>
-            <img src={four} alt="/" className='float-right w-full h-full' />
+          <div className='md:mt-16'>
+            <img src={four} alt="/"  className=' relative object-cover -bottom-10 w-full'/>
           </div>
         </div>
 
         {/* 5th */}
         <div className='grid grid-cols-2 bg-gradient-to-r  max-h-96 overflow-hidden float from-slate-400 to-slate-300 rounded-md py-3 px-4 items-center relative' >
           <div className='w-[130%]' >
-            <h2 className='text-4xl font-black text-white' >Pick up and Drop</h2>
-            <p className='py-6 text-gray-700' >We make life easier for you and your furry companion by offering reliable and convenient pet transportation services</p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins'>Pick up <br />  <br className='hidden' /> and Drop</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl md:mb-2'>We make life easier for you and your furry companion by offering reliable and convenient pet transportation services</p>
           </div>
-          <div>
-            <img src={five} alt="/" className='relative -right-6 mt-10 md:mt-[-10px] md:mr-[-10px]' />
+          
+          <div className='md:mt-16 '>
+            <img src={five} alt="/"  className='relative w-[70%] md:mt-[-10px] md:-right-20 -right-10 '/>
           </div>
         </div>
 
@@ -87,8 +88,8 @@ const Services = () => {
             <img src={six} alt="/" className='relative -left-3 object-contain' />
           </div>
           <div className='w-[130%]  relative left-[-30%] mx-2' >
-            <h2 className='text-4xl font-black text-white'>Pet Bakery</h2>
-            <p className='py-6 text-gray-700' >Treat your pet with delicious and healthy homemade pet treats from our artisan pet bakery</p>
+            <h2 className='text-3xl md:text-[70px] font-bold text-white font-Poppins' >Pet Bakery</h2>
+            <p className='mt-6 md:mt-10 space-y-[30px] space-x-1 text-[#454647] font-Poppins text-sm md:text-2xl'  >Treat your pet with delicious and healthy homemade pet treats from our artisan pet bakery</p>
           </div>
         </div>
 
